94bc7128db64ad551f248c815c9cd23b3c313a4d
dougbtv <dosmith@redhat.com>
2023-01-05 12:31:03 -0500

    [increment] stubs a CRD yaml

--- cmd/surveyor.go
+++ cmd/surveyor.go
@@ -20,7 +20,7 @@ func main() {
 		nil,
 		cmdDel,
 		cniVersion.PluginSupports("0.1.0", "0.2.0", "0.3.0", "0.4.0"),
-		"Chainsaw CNI "+version.Version)
+		"Surveyor CNI "+version.Version)
 }
 
 func cmdAdd(args *skel.CmdArgs) error {
